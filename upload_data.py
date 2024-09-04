f66607cd913d395869c97c3a1cea7e9de257c5b1
Ahmed Emad <ahmed_emad1@aucegypt.edu>
2024-09-04 15:28:49 +0300

    First commit

---
+++ upload_data.py
@@ -0,0 +1,89 @@
+import os
+from time import time
+import pandas as pd
+import argparse
+from sqlalchemy import create_engine
+
+
+
+parser = argparse.ArgumentParser(description='ingest CSV data')
+parser.add_argument('user',help='username for postgres')
+parser.add_argument('password',help='password for postgres')
+parser.add_argument('host',help='host name for postgres')
+parser.add_argument('port',help='port name for postgres')
+parser.add_argument('db',help='db name for postgres')
+parser.add_argument('table_name',help='name of table where data will be written')
+parser.add_argument('url',help='url for data file')
+
+def main(params):
+
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'yellow_taxi.csv'
+
+    os.system(f'wget {url} -O {csv_name}')
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+
+    df_iter = pd.read_csv(csv_name,iterator=True, chunksize = 100000)
+
+
+
+    df = next(df_iter)
+
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    import pandas as pd
+    from time import time
+
+    while True:
+        try:
+            df = next(df_iter)
+        except StopIteration:
+            print("All chunks processed.")
+            break
+
+        t_start = time()
+        
+        # Convert datetime columns
+        df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+        df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+
+        # Append to SQL table
+        df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append', index=False)
+
+        t_end = time()
+        print(f'Time for appending chunk: {t_end - t_start:.3f} seconds')
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ingest CSV data')
+ 
+    parser.add_argument('--user',help='username for postgres')
+    parser.add_argument('--password',help='password for postgres')
+    parser.add_argument('--host',help='host name for postgres')
+    parser.add_argument('--port',help='port name for postgres')
+    parser.add_argument('--db',help='db name for postgres')
+    parser.add_argument('--table_name',help='name of table where data will be written')
+    parser.add_argument('--url',help='url for data file')
+
+    args = parser.parse_args()
+
+    main(args)
